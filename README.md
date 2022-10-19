ddc7d1bfdb09e74cacc71b287d07827e18efa98d
Leila Alves Evangelista <leila.ae@gmail.com>
2022-10-19 00:50:36 -0300

    javascript em construção conforme readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-Projeto para estudos de HTML e CSS 
+Projeto para estudos de HTML, CSS e Javascript.
+
+HTML e CSS:
+https://goo.gl/yMrCaf
+
+
+Javascript:
+
+ok / 1 - Validar o formulário para que todos os campos sejam preenchidos.
+
+2 - Adicionar uma máscara no campo “Valor” para que apenas números sejam preenchidos e com a formatação correta. (Padrão: 10,90)
+
+3 - Ao adicionar uma nova transação, persistir no Local Storage e já atualizar a lista com o extrato. Atualizar também o cálculo apresentado.
+
+4 - Ao clicar no link “Limpar dados”, apresentar uma mensagem de confirmação e em seguida apagar as informações, atualizando a lista.
---
+++ script.js
@@ -0,0 +1,26 @@
+//máscara de preço//
+
+function formatoPreco() {
+    let valor = 0;
+    const formatoValor = valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    return formatoValor;
+}
+
+
+//validando os inputs//
+
+function validarBotaoAdicionar() {
+
+    const mercadoria = document.getElementById('mercadoria').value
+    const preco = document.getElementById('preco').value
+
+    if (mercadoria == ""|| preco == "0" ){
+            return alert("Preencha os campos para seguir.")
+        }else {
+            const novoProduto = localStorage.getItem("primeiraLinhaTabela");
+        console.log(mercadoria, preco)
+            return novoProduto;
+    }
+}
+
+//Limpar dados//
