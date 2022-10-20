98246056ea6ab8518531215de3ef8f98203eecff
Leila Alves Evangelista <leila.ae@gmail.com>
2022-10-20 01:43:04 -0300

    Extrato em construção js

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
-//máscara de preço//
+//validando os inputs - ítem 01 do readme//
+
+const novaTransacaoUL = document.querySelector('#novaTransacaoUL');
+
+const novoRegistro = [
+    {
+        id: () => {
+            const dateString = Date.now().toString(36);
+            const randomness = Math.random().toString(36);
+            return dateString + randomness;
+        },
+        mercadoria: true,
+        preco: true
+    }
+]
+
+const extrato = transacao => {
+    const operador = transacao.amount < 0 ? '-' : '+';
+    const CSSClass = transacao.amount < 0 ? 'minus' : 'plus';
+    const operadorNegativo = Math.abs(transacao.amount);
+    const li = document.createElement('li');
+
+    li.classList.add(CSSClass);
+    li.innerHTML = `
+        ${transacao.name} <span>${operador} R$ ${operadorNegativo} </span>
+    `
+    novaTransacaoUL.prepend(li); //mostra a transação + recente primeiro//
+}
+
+const addTransacaoDom = () => {
+    novoRegistro.forEach(extrato);
+}
+
+addTransacaoDom()
+
+
+//máscara de preço - ítem 02 do readme//
 
 function formatoPreco() {
     let valor = 0;
@@ -6,20 +42,20 @@ function formatoPreco() {
     return formatoValor;
 }
 
-
-//validando os inputs//
+//Armazenar em localStorage - ítem 03 do readme//
 
 function validarBotaoAdicionar() {
-    const mercadoria = document.getElementById('mercadoria').value
-    const preco = document.getElementById('preco').value
+    const mercadoria = document.getElementById('mercadoria').value;
+    const preco = document.getElementById('preco').value;
 
-    if (mercadoria == ""|| preco == "0" ){
-            return alert("Preencha os campos para seguir.")
-        }else {
-            const novoProduto = localStorage.getItem("primeiraLinhaTabela");
+    if (mercadoria == "" || preco == "0") {
+        return alert("Preencha os campos para seguir.");
+    } else {
+        const novoProduto = localStorage.getItem("novaTransacaoUL");
         console.log(mercadoria, preco)
-            return novoProduto;
+        return novoProduto;
     }
 }
 
-//Limpar dados//
+//Limpar dados - ítem 04 do readme//
+
