50ee58838ac7a69596e2f999bcd4b2e16417d40f
Leila Alves Evangelista <leila.ae@gmail.com>
2022-11-23 00:57:30 -0300

    add item funcionando

--- script.js
+++ script.js
@@ -1,71 +1,43 @@
-var coletaDados = [{
-    "transacao": "venda",
-    "mercadoria": "mouse",
-    "valor": 655.89
-},
-{
-    "transacao": "compra",
-    "mercadoria": "mesa",
-    "valor": 500.99
-},
-{
-    "transacao": "venda",
-    "mercadoria": "mochila",
-    "valor": 265.89
-}
-]
+var listaExtrato = [];
 
-
-var listaExtrato = [{
-    "transacao": "",
-    "mercadoria": "",
-    "valor": 0
-}];
-
-localStorage.setItem('chaveTransacao', JSON.stringify(coletaDados));  //setItem salva no localStorage (chave e valor)// 
+localStorage.setItem('chaveTransacao', JSON.stringify(listaExtrato));  //setItem salva no localStorage (chave e valor)// 
 
 var stringDados = localStorage.getItem('chaveTransacao') //getItem pega o valor do da chave utilizada// 
-console.log(stringDados) //até aqui está FUNCIONANDO//
 
 if (stringDados) {
-    listaExtrato = JSON.parse(stringDados)
-
-    console.log(listaExtrato) //até aqui está FUNCIONANDO, traz o array de objetos //
-
+    listaExtrato = JSON.parse(stringDados) //até aqui está FUNCIONANDO, traz o array de objetos //
 }
 
 /////////////////////////////////////formato do extrato////////////////////////////////////////////////
 
 function formatoExtrato() {
-    
-    document.querySelector('#extratoTBody', 'tfoot').innerHTML = ''
 
+    document.querySelector('#extratoTBody').innerHTML = ''
     let total = 0
 
     for (produto of listaExtrato) {
 
         total += produto.valor * (produto.transacao == 'venda' ? 1 : -1)
-        //transação soma ou subtrai - FUNCIONANDO mas sem formatação//
-        
+        //transação soma ou subtrai - FUNCIONANDO
 
         document.querySelector('#extratoTBody').innerHTML += `
         <tr>
             <td>${produto.transacao.value == 'venda' ? '+' : '-'} </td>
             <td>${produto.mercadoria}</td>
-            <td>${produto.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL', maximumFractionDigits: 2 }) }</td> 
+            <td>${produto.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL', maximumFractionDigits: 2 })}</td> 
         </tr>
         `
         //máscara de valor acima - FUNCIONANDO//
-        document.getElementById('#extratoTBody')
-
     }
 
+
     //tfoot com lucro ou prejuízo - FUNCIONANDO//
+    document.querySelector('table tfoot').innerHTML = ''
     document.querySelector('table tfoot').innerHTML += `
         <tr>
             <td></td>
             <td>Total</td>
-            <td>${total < 0 ? total * -1 : total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL', maximumFractionDigits: 2 }) } </td>
+            <td>${produto.valor < 0 ? total * -1 : total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL', maximumFractionDigits: 2 })} </td>
         </tr>
         
         <tr>
@@ -75,7 +47,6 @@ function formatoExtrato() {
         </tr>
         `;
 
-    document.getElementById('#tfooter')
 }
 
 
@@ -83,8 +54,13 @@ formatoExtrato()
 
 /////////////////////////////////////fim do formato do extrato////////////////////////////////////////////
 
-function adicionarItem(listaExtrato) {
-    listaExtrato.push(coletaDados);
+function adicionarItem() {
+
+    listaExtrato.push({
+        transacao: document.getElementById('transacao').value,
+        mercadoria: document.getElementById('mercadoria').value,
+        valor: parseFloat(document.getElementById('valor').value.replaceAll('.', '').replace(',', '.'))
+    });
 
     localStorage.setItem('chaveTransacao', JSON.stringify(listaExtrato))
 
@@ -92,3 +68,11 @@ function adicionarItem(listaExtrato) {
 }
 
 adicionarItem()
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//-> arrumar máscara de preço no input de valor
+//-> ajustar os sinais aritméticos do cáculo no extrato
+//-> ler mais sobre localStorage  
+//-> criar validação dos inputs
+//-> se der tempo, ajustar a tabela no padrão do figma 
